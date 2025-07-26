8d85b82268e5e0a39aa95b4c9f02c93519022f7e
Senoquese <hugoliveira_mw@hotmail.com>
2025-07-26 19:16:04 +0000

    Update embed.js

--- embeds/embed.js
+++ embeds/embed.js
@@ -1,13 +1,28 @@
 fetch("https://raw.githubusercontent.com/Senoquese/VTuberRacingSite/refs/heads/main/embeds/header.html")
 	.then(response => response.text())
-	.then(data => $(header)[0].innerHTML = data)
+	.then(data => {
+		$(header)[0].innerHTML = data
+		var headerBar = $(header)
+		headerBar.css("position", "fixed")
+		headerBar.css("top", "0px")
+		headerBar.css("height", "2.25rem")
+		headerBar.css("width", "100%")
+		headerBar.css("z-index", "100")
 
-var headerBar = $(header)
-headerBar.css("position", "fixed")
-headerBar.css("top", "0px")
-headerBar.css("height", "2.25rem")
-headerBar.css("width", "100%")
-headerBar.css("z-index", "100")
+		$(function(){
+			const gitscripts = document.createElement('script');
+			fetch("https://raw.githubusercontent.com/Senoquese/VTuberRacingSite/refs/heads/main/embeds/scripts.js")
+				.then(response => response.text())
+				.then(data => gitscripts.innerHTML = data)
+			document.body.appendChild(gitscripts);
+
+			const trophiesscript = document.createElement('script');
+			fetch("https://raw.githubusercontent.com/Senoquese/VTuberRacingSite/refs/heads/main/embeds/trophies.js")
+				.then(response => response.text())
+				.then(data => trophiesscript.innerHTML = data)
+			document.body.appendChild(trophiesscript);
+		})
+	})
 
 const overlays = document.createElement('div');
 fetch("https://raw.githubusercontent.com/Senoquese/VTuberRacingSite/refs/heads/main/embeds/overlays.html")
@@ -26,17 +41,3 @@ fetch("https://raw.githubusercontent.com/Senoquese/VTuberRacingSite/refs/heads/m
 	.then(response => response.text())
 	.then(data => gitstyleoverlay.innerHTML = data)
 	document.head.appendChild(gitstyleoverlay);
-
-$(function(){
-	const gitscripts = document.createElement('script');
-	fetch("https://raw.githubusercontent.com/Senoquese/VTuberRacingSite/refs/heads/main/embeds/scripts.js")
-		.then(response => response.text())
-		.then(data => gitscripts.innerHTML = data)
-	document.body.appendChild(gitscripts);
-
-	const trophiesscript = document.createElement('script');
-	fetch("https://raw.githubusercontent.com/Senoquese/VTuberRacingSite/refs/heads/main/embeds/trophies.js")
-		.then(response => response.text())
-		.then(data => trophiesscript.innerHTML = data)
-	document.body.appendChild(trophiesscript);
-})
