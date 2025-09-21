4e083db662aeb0c5d8f9bccaefc5001b72d3a930
Senoquese <hugoliveira_mw@hotmail.com>
2025-09-21 21:29:49 +0000

    browser

--- chillaProfileTest/chillaProfileTest.service.worker.js
+++ chillaProfileTest/chillaProfileTest.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1758490003|17057372350';
+const CACHE_VERSION = '1758490179|17233334941';
 /** @type {string} */
 const CACHE_PREFIX = 'The Chilla Races-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
