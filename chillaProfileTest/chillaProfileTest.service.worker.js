7cb585574dd12745a98aee31f9e4a50fa1f8b5bd
Senoquese <hugoliveira_mw@hotmail.com>
2025-09-21 19:29:00 +0000

    threads??

--- chillaProfileTest/chillaProfileTest.service.worker.js
+++ chillaProfileTest/chillaProfileTest.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1758482895|9950110702';
+const CACHE_VERSION = '1758482932|9986388098';
 /** @type {string} */
 const CACHE_PREFIX = 'The Chilla Races-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
